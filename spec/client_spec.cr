0c2401c1f0b40747746e4566ccb677801e87bd69
Chris Blunt <dev@chrisblunt.me>
2025-09-04 13:54:19 +0700

    Add extra specs for connection error handling

--- spec/client_spec.cr
+++ spec/client_spec.cr
@@ -19,4 +19,20 @@ describe Pop3Client::Client do
       fake.close
     end
   end
+
+  it "errors if quitting while not connected" do
+    client = Pop3Client::Client.new("127.0.0.1", 12345)
+    expect_raises(Pop3Client::NotConnectedError) { client.quit }
+  end  
+
+  it "errors if connecting while already connected" do
+    fake = TestSupport::FakePOP3.new("+OK hello")
+    begin
+      client = Pop3Client::Client.new("127.0.0.1", fake.port)
+      client.connect
+      expect_raises(Pop3Client::AlreadyConnectedError) { client.connect }
+    ensure
+      fake.close
+    end
+  end
 end
